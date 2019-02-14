cbef21440198738d37b666f2f2cb1d987c7aad65
Yusrina Nor <Yusrina Nor>
2019-02-14 23:43:38 +0800

    add more content, fix images overflow and add animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    AOS.init({ once: true, duration: 700 });
 
     /* ======= Scrollspy ======= */
    $('body').scrollspy({ target: '#page-nav-wrapper', offset: 100});
